96e13bdd3e3330449b3ee7f19272abd430cb07d4
LuisaLara18 <lel230002@utdallas.edu>
2025-05-05 12:25:30 -0500

    Created trash class so trash pile is able to span randomly within game screen.

--- src/project.py
+++ src/project.py
@@ -3,16 +3,28 @@ from pygame.math import Vector2
 import random
 import sys
 
-class Fruit():
+class AppleFruit():
 
     def __init__(self):
         self.x = random.randint(0, 19)
         self.y = random.randint(0, 19)
         self.pos = Vector2(self.x, self.y)
 
-    def draw_fruit(self):
-        fruit_rect = pygame.Rect(self.pos.x * 40, self.pos.y * 40, 40, 40)
-        pygame.draw.rect(screen, (126, 166, 114) , fruit_rect)
+    def draw_apple(self):
+        apple_rect = pygame.Rect(self.pos.x * 40, self.pos.y * 40, 40, 40)
+        pygame.draw.rect(screen, (126, 166, 114) , apple_rect)
+
+
+class TrashPile():
+
+    def __init__(self):
+        self.x = random.randint(0, 19)
+        self.y = random.randint(0, 19)
+        self.pos = Vector2(self.x, self.y)
+
+    def draw_trash(self):
+        trash_rect = pygame.Rect(self.pos.x * 40, self.pos.y * 40, 40, 40)
+        pygame.draw.rect(screen, (128, 128, 128), trash_rect)
 
 
 class Snake():
@@ -25,13 +37,15 @@ class Snake():
             block_rect = pygame.Rect(block.x * 40, block.y * 40, 40, 40)
             pygame.draw.rect(screen, (183, 111, 122), block_rect)
 
+
 pygame.init()
 cell_size = 40
 cell_number = 20
 screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
 clock = pygame.time.Clock()
 
-fruit = Fruit()
+apple = AppleFruit()
+trash = TrashPile()
 snake = Snake()
 
 while True:
@@ -41,7 +55,8 @@ while True:
             sys.exit()
         
     screen.fill(pygame.Color(175, 215, 70))
-    fruit.draw_fruit()
+    apple.draw_apple()
+    trash.draw_trash()
     snake.draw_snake()
     pygame.display.update()
     clock.tick(60)
