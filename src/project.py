7f09c6842aaa8dfd0a4f6ba2b85cecc774ac927b
LuisaLara18 <lel230002@utdallas.edu>
2025-05-05 12:03:51 -0500

    Added the illusion of a grid by incorporating cell_size and multiplying it with the x and y positions of self

--- src/project.py
+++ src/project.py
@@ -1,16 +1,36 @@
 import pygame
+from pygame.math import Vector2
 import sys
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((1000, 800))
+class Fruit():
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        pygame.display.update()
+    def __init__(self):
+        self.x = 5
+        self.y = 10
+        self.pos = Vector2(self.x, self.y)
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size, cell_size, cell_size)
+        pygame.draw.rect(screen, (126, 166, 114) , fruit_rect)
+
+pygame.init()
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
+clock = pygame.time.Clock()
+
+fruit = Fruit()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        
+    screen.fill(pygame.Color(175, 215, 70))
+    fruit.draw_fruit()
+    pygame.display.update()
+    clock.tick(60)
 
 if __name__ == "__main__":
     main()
