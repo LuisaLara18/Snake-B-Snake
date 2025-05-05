ad7a3df8e1ef8eb21dce6be8ad08bfc2811b8e29
LuisaLara18 <lel230002@utdallas.edu>
2025-05-05 12:14:17 -0500

    Created snake class and allowed it to be drawn within game screen.

--- src/project.py
+++ src/project.py
@@ -1,17 +1,29 @@
 import pygame
 from pygame.math import Vector2
+import random
 import sys
 
 class Fruit():
 
     def __init__(self):
-        self.x = 5
-        self.y = 10
+        self.x = random.randint(0, 19)
+        self.y = random.randint(0, 19)
         self.pos = Vector2(self.x, self.y)
 
     def draw_fruit(self):
-        fruit_rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size, cell_size, cell_size)
+        fruit_rect = pygame.Rect(self.pos.x * 40, self.pos.y * 40, 40, 40)
         pygame.draw.rect(screen, (126, 166, 114) , fruit_rect)
+
+
+class Snake():
+
+    def __init__(self):
+        self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
+
+    def draw_snake(self):
+        for block in self.body:
+            block_rect = pygame.Rect(block.x * 40, block.y * 40, 40, 40)
+            pygame.draw.rect(screen, (183, 111, 122), block_rect)
 
 pygame.init()
 cell_size = 40
@@ -20,6 +32,7 @@ screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_si
 clock = pygame.time.Clock()
 
 fruit = Fruit()
+snake = Snake()
 
 while True:
     for event in pygame.event.get():
@@ -29,6 +42,7 @@ while True:
         
     screen.fill(pygame.Color(175, 215, 70))
     fruit.draw_fruit()
+    snake.draw_snake()
     pygame.display.update()
     clock.tick(60)
 
